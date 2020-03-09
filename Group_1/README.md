3167d6b2c371c2e6169a58260d197fdb936415d7
madhavmk <madhavkashyap99@gmail.com>
2020-03-09 19:46:08 +0530

    multiple groups and multiple rooms

---
+++ Group_1/README.md
@@ -0,0 +1,7 @@
+Exam Seating Allocation Research using Natural Based Algorithms. 
+Research work by:
+Madhav M K,
+Gurav C G,
+Thejas
+
+Swap mutation seems to work the best among all
---
+++ all_group_call.py
@@ -0,0 +1,39 @@
+import os
+import copy
+import subprocess
+
+subfolders_list = [ f.path for f in os.scandir("./") if f.is_dir() ]
+
+group_list = copy.deepcopy(subfolders_list)
+
+try:
+    group_list.remove('./__pycache__')
+except:
+    pass
+try:
+    group_list.remove('./.git')
+except:
+    pass
+
+print('Groups are : ',group_list)
+
+processes=[]
+
+for group in group_list:
+    command = "cd " + group + " && " + "python seat_allocation.py > out.txt"
+    process = subprocess.Popen(command, shell=True)
+    processes.append(process)
+
+output = [p.wait() for p in processes]
+
+"""
+print('Started group 1')
+#os.system('cmd /c "cd Group_1  && python seat_allocation.py > out.txt"')
+p1 = subprocess.Popen( "cd Group_1  && python seat_allocation.py > out.txt")
+print('Started group 2')
+#os.system('cmd /c "cd Group_2  && python seat_allocation.py > out.txt"')
+p2 = subprocess.Popen( "cd Group_2  && python seat_allocation.py > out.txt")
+print('Started group 3')
+#os.system('cmd /c "cd Group_3  && python seat_allocation.py > out.txt"')
+p3 = subprocess.Popen( "cd Group_3  && python seat_allocation.py > out.txt")
+"""
